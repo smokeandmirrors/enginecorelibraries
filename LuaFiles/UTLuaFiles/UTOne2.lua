15a9d9403de361b2a491f8f38f25f97325d1d7fa
smokeandmirrorsdevelopment <smokeandmirrorsdevelopment@gmail.com>
2011-12-21 17:38:33 -0800

    More compatibility

--- LuaFiles/UTLuaFiles/UTOne2.lua
+++ LuaFiles/UTLuaFiles/UTOne2.lua
@@ -1,5 +1,5 @@
-_ENV = deprecatedNaughtyModule(..., package.seeall)
-
+--[[ _ENV = --]]deprecatedNaughtyModule(..., package.seeall)
+--[[ _ENV = --]]
 One2.decrement = function(i) 
 	return i - 1 
 end
